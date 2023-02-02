b266d2d6a7d05b7663487fb15d873acdf8207c72
Mikhail Valerko <mixelv@mail.ru>
2023-02-02 21:01:33 +0100

    Initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $ru.netology.mvalerko;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
